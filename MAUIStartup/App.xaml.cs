32fdbd2f62b8dec4c2ba963d991efbf2c2cc061f
Mahdi <137503197+Mahdi-Yadi@users.noreply.github.com>
2024-12-21 22:48:35 +0330

    Add project files.

---
+++ MAUIStartup/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MAUIStartup
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ MAUIStartup/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUIStartup
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
